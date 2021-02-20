eefb9d44b57755d90baa0bc713a467866ff3c614
kavin <kavinkumarnkm007@gmail.com>
2021-02-20 18:28:22 +0530

    feat[operations_maintananace]: Automate the shift type creation while creating the shift

--- operations_maintanance/operations_and_maintanance/doctype/shift/shift.py
+++ operations_maintanance/operations_and_maintanance/doctype/shift/shift.py
@@ -3,8 +3,22 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Shift(Document):
-	pass
+	def validate(self):
+		for shift in self.shift_timings:
+			shift_type_doc = frappe.new_doc("Shift Type")
+			shift_type_doc.name  = self.project_number+"-"+shift.shift_name
+			shift_type_doc.__newname = self.project_number+"-"+shift.shift_name
+			shift_type_doc.project_number  = self.project_number
+			if frappe.db.exists("Shift Type",{"name":self.project_number+"-"+shift.shift_name}):
+				shift_type_doc = frappe.get_doc("Shift Type",self.project_number+"-"+shift.shift_name)
+			shift_type_doc.start_time = shift.start_time
+			shift_type_doc.end_time = shift.end_time
+			shift_type_doc.save()
+
+			
+			
+
