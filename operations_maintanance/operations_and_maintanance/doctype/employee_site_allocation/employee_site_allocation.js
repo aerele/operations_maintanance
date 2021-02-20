b0320a9f07463e19cabb76d8f6e0dd3b857a708a
venkat102 <venkatesharunachalam659@gmail.com>
2021-02-20 15:49:56 +0530

    fix[employee site allocation]: remove js auto populating code fix[employee site aloocation]: delete doctype

--- operations_maintanance/operations_and_maintanance/doctype/employee_site_allocation/employee_site_allocation.js
+++ operations_maintanance/operations_and_maintanance/doctype/employee_site_allocation/employee_site_allocation.js
@@ -2,20 +2,5 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Site Allocation', {
-	project_number : function(frm){
-		if (frm.doc.project_number != null){
-			frappe.call({
-				method: "frappe.client.get",
-				args: {
-					doctype: "Site Creation",
-					name: frm.doc.project_number,
-				},
-				callback(r) {
-					if(r.message) {
-						frm.set_value("site_name",r.message["site_name"]);
-					}
-				}
-			});
-		}
-	}
+	
 });
